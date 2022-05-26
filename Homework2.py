1c4fa4f395d7f3349a61fe50d086cb303adcc512
Alexx <al_nesteov@inbox.ru>
2022-05-26 16:11:00 +0300

    homework 2 in python

---
+++ Homework2.py
@@ -0,0 +1,32 @@
+print ('Задача 1. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N ')
+
+from math import factorial
+n = int(input ('введите N: '))
+List = []
+j= 1
+for i in range(n):
+    List.append(factorial(j))
+    j += 1
+print (List)
+
+input()
+
+print ('Задача 3. Алгоритм перемешивания списка ')
+import random
+print ('исходный список:', end = '     ')
+list = [1, 'world', -100, 2.5, 'привет']
+print (list)
+random.shuffle(list)
+print ('перемешанный список:', list)
+
+input()
+
+print ('Задача 2. Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму. Нахождение экспоненты')
+n = int(input ('введите N: '))
+Dict = {}
+rezult = 0
+for i in range(1, n+1):
+    Dict[i] = round ((1+1/(i))**i, 4)
+    rezult += Dict[i]
+print (Dict)
+print (f'сумма значений =  {rezult}; Dict[{n}] = {Dict[n]}')
