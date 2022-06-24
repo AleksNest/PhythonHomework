9215708e9da10057a7099b9355f06effcb76c21f
Alexx <al_nesteov@inbox.ru>
2022-06-24 23:54:37 +0300

    homework 9  bot calculation correction

--- homework_9.py
+++ homework_9.py
@@ -12,10 +12,12 @@ DATE1 = 3
 DATE2 = 4
 DATE3 = 5
 DATE4 = 6
+
     
   
 def start(update, context):
     context.bot.send_message(update.effective_chat.id, 'КАЛЬКУЛЯТОР  ')
+    context.bot.send_message(update.effective_chat.id, '/start - запуск калькулятора')
     context.bot.send_message(update.effective_chat.id, 'выбирете тип чисел: 1 - рациональные, 2 - комплексные  ')
     
     return START
@@ -26,6 +28,7 @@ def input_data_0(update, context):
     type_number = update.message.text
     if type_number == '1':
         context.bot.send_message(update.effective_chat.id, 'Введи арифметическое выражение без знака "=": ')
+        
     else:
         context.bot.send_message(update.effective_chat.id, 'Введи действительную часть первого числа: ')
 
@@ -40,7 +43,7 @@ def input_data_1(update, context):
         print(calc)
         rezult = eval (calc)
         context.bot.send_message(update.effective_chat.id, f'результат: {rezult}')
-
+        return ConversationHandler.END
     else:
         first_number_dv = update.message.text
         print(first_number_dv)
@@ -118,17 +121,18 @@ def operation(update, context):
 
 
 def cancel(update, context):
+    context.bot.send_message(update.effective_chat.id, 'выход из калькулятора')
     return ConversationHandler.END
 
  
 start_handler = CommandHandler('start', start)
+cancel_handler = CommandHandler('cancel', cancel)
 zero_handler = MessageHandler(Filters.text, input_data_0)
 first_handler = MessageHandler(Filters.text, input_data_1)
 second_handler = MessageHandler(Filters.text, input_data_2)
 third_handler = MessageHandler(Filters.text, input_data_3)
 fourth_handler = MessageHandler(Filters.text, input_data_4)
 operation_handler = MessageHandler(Filters.text, operation)
-cancel_handler = CommandHandler('cancel', cancel)
 
 
 conv_handler = ConversationHandler(entry_points=[start_handler],
@@ -137,10 +141,10 @@ conv_handler = ConversationHandler(entry_points=[start_handler],
                                             DATE1: [second_handler],
                                             DATE2: [third_handler],
                                             DATE3: [fourth_handler],
-                                            DATE4: [operation_handler]},
+                                            DATE4: [operation_handler],},
                                     fallbacks=[cancel_handler])
 
 dispatcher.add_handler(conv_handler) 
-
+print ('server start')
 updater.start_polling()
 updater.idle()
